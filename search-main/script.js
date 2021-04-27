b43fc0bf303fd78c079e5887bca19d3f4b547195
charliekaka <70217764+charliekaka@users.noreply.github.com>
2021-04-27 11:19:15 +0200

    Add files via upload

---
+++ search-main/script.js
@@ -0,0 +1,40 @@
+// för google knapp
+function Submit(){
+    var i = document.getElementById('search').value;
+    const search = "https://www.google.com/search?q="
+    console.log(i)
+    window.open(search + i)
+}
+
+// för slider
+var isDark = false;
+function Darkmode(){
+    const root = document.querySelector(':root');
+    if(isDark == false){
+        console.log("white to black")
+        //ändrar css variable
+        root.style.setProperty('--white', '#333333');
+        root.style.setProperty('--whitedim', '#1f1f1f');
+        root.style.setProperty('--black_text', 'white');
+        root.style.setProperty('--box_Shadow', 'black');
+        // Ändrar bild
+        document.getElementById("light_logo").style.display = "none"
+        document.getElementById("dark_logo").style.display = "block"
+        isDark = true;
+    }
+    else if(isDark == true){
+        console.log("black to white")
+        //ändrar css variable
+        root.style.setProperty('--white', 'lightgray');
+        root.style.setProperty('--whitedim', 'white');
+        root.style.setProperty('--black_text', 'black');
+        root.style.setProperty('--box_Shadow', '#7c7c7c');
+        // Ändrar bild
+        document.getElementById("light_logo").style.display = "block"
+        document.getElementById("dark_logo").style.display = "none"
+        isDark = false;
+    }
+}
+
+
+
